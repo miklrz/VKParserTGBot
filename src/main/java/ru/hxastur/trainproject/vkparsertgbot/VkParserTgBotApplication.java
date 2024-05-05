ba414e8e5a9abaa5cf70dd7fa6499f56e4ccaea1
miklrz <arz.mikhail@gmail.com>
2024-05-05 16:39:10 +0300

    Initial commit

---
+++ src/main/java/ru/hxastur/trainproject/vkparsertgbot/VkParserTgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.hxastur.trainproject.vkparsertgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkParserTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkParserTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hxastur/trainproject/vkparsertgbot/VkParserTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hxastur.trainproject.vkparsertgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkParserTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
